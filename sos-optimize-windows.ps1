da5282772b67465b95d485b23bc2e5856003cc60
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-02-08 21:02:37 +0000

    Update sos-optimize-windows.ps1

--- sos-optimize-windows.ps1
+++ sos-optimize-windows.ps1
@@ -1155,6 +1155,9 @@ Start-Job -Name "Enable Privacy Settings" -ScriptBlock {
     Write-Output "removing skype App"
     Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -Like "Microsoft.SkypeApp" } | ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName }
     ##General VM Optimizations
+    #Change TTL for ISP throttling workaround
+    int ipv4 set glob defaultcurhoplimit=65
+    int ipv6 set glob defaultcurhoplimit=65
     #Auto Cert Update
     Write-Output "Auto Cert Update"
     Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\SystemCertificates\AuthRoot" -Name DisableRootAutoUpdate -Type "DWORD" -Value 0 -Force
